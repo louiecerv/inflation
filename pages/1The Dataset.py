98c785082b0089c3ac7131b3e359396d133d9cdc
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-03-24 11:54:07 +0000

    Save

--- pages/1The Dataset.py
+++ pages/1The Dataset.py
@@ -36,7 +36,7 @@ def app():
         st.write(text)
 
     df = pd.read_csv('e-banking3.csv', header=0)
-    df = df.drop('Usage', axis = 1)
+    #df = df.drop('Usage', axis = 1)
 
     # Shuffle the DataFrame
     df = df.sample(frac=1)
@@ -61,10 +61,11 @@ def app():
     le_list = []  # Create an empty array to store LabelEncoders
     # Loop through each column name
     for cn in column_names:
-        le = LabelEncoder()  # Create a new LabelEncoder for each column
-        le.fit(df[cn])  # Fit the encoder to the specific column
-        le_list.append(le)  # Append the encoder to the list
-        df[cn] = le.transform(df[cn])  # Transform the column using the fitted encoder
+        if cn != "Usage":
+            le = LabelEncoder()  # Create a new LabelEncoder for each column
+            le.fit(df[cn])  # Fit the encoder to the specific column
+            le_list.append(le)  # Append the encoder to the list
+            df[cn] = le.transform(df[cn])  # Transform the column using the fitted encoder
 
     # save the label encoder to the session state
     st.session_state["le_list"] = le_list
